7fbdbf751930e52f9702bc00ce0ab93180feb199
shivammaheshwari9837 <shivammaheshwari9837@gmail.com>
2023-06-19 23:30:25 +0530

    Integrated the Map View and also permission manager for a user

--- NearMe/ViewController.swift
+++ NearMe/ViewController.swift
@@ -6,14 +6,88 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    private var locationManager: CLLocationManager?
+    
+    private let mapView: MKMapView = {
+        let map = MKMapView()
+        map.showsUserLocation = true
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
+    
+    private let searchTextField: UITextField = {
+        let textField = UITextField()
+        textField.layer.cornerRadius = 10
+        textField.clipsToBounds = true
+        textField.backgroundColor = UIColor.white
+        textField.placeholder = "Search"
+        textField.leftView = UIView.init(frame: .init(x: 0, y: 0, width: 10, height: 0))
+        textField.leftViewMode = .always
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        commonInit()
     }
 
+}
 
+private extension ViewController {
+    func commonInit() {
+        setupUI()
+        setupHierarchy()
+        setupConstraints()
+    }
+    
+    func setupUI() {
+        setupLocationAccess()
+    }
+    
+    func setupHierarchy() {
+        view.addSubview(mapView)
+        view.addSubview(searchTextField)
+    }
+    
+    func setupConstraints() {
+        
+        searchTextField.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        searchTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        searchTextField.widthAnchor.constraint(equalToConstant: view.bounds.size.width / 1.2).isActive = true
+        searchTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 60).isActive = true
+        searchTextField.returnKeyType = .search
+        
+        mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
+
+private extension ViewController {
+    func setupLocationAccess() {
+        self.locationManager = CLLocationManager()
+        self.locationManager?.delegate = self
+        
+        self.locationManager?.requestWhenInUseAuthorization()
+        self.locationManager?.requestAlwaysAuthorization()
+        self.locationManager?.requestLocation()
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
 }
 
